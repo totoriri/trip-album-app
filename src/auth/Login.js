d391ab37943eab863d979a6b46ffbffb2499bd27
Risa Takata <beat1422@icloud.com>
2020-10-02 00:17:32 -0700

    UPDATE: style login page using material ui

--- src/auth/Login.js
+++ src/auth/Login.js
@@ -2,8 +2,45 @@ import React, { useContext } from "react";
 import { withRouter } from "react-router";
 import { Link } from "react-router-dom";
 import { AuthContext } from "./AuthProvider";
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.primary.main,
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+}));
 
 const Login = ({ history }) => {
+
+  const classes = useStyles();
+
   const { login } = useContext(AuthContext);
 
   const handleSubmit = event => {
@@ -13,21 +50,65 @@ const Login = ({ history }) => {
   };
 
   return (
-    <div>
-      <h1>Log in</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email
-          <input name="email" type="email" placeholder="Email" />
-        </label>
-        <label>
-          Password
-          <input name="password" type="password" placeholder="Password" />
-        </label>
-        <button type="submit">Log in</button>
-        <Link to="/signup">Go to signup page</Link>
-      </form>
-    </div>
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className={classes.paper}>
+      <Avatar className={classes.avatar}>
+          <LockOutlinedIcon />
+        </Avatar>
+      <Typography component="h1" variant="h5">Log in</Typography>
+      <form onSubmit={handleSubmit} className={classes.form}>
+          {/* <input name="email" type="email" placeholder="Email" /> */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+              name="email"
+              type="email"
+            autoComplete="email"
+            autoFocus
+          />
+          {/* <input name="password" type="password" placeholder="Password" /> */}
+          <TextField
+            variant="outlined"
+            margin="normal"
+            required
+            fullWidth
+            name="password"
+            label="Password"
+            type="password"
+            id="password"
+            autoComplete="current-password"
+            />
+          {/* <button type="submit">Log in</button> */}
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className={classes.submit}
+            >
+            Log In
+            </Button>
+          {/* <Link to="/signup">Go to signup page</Link> */}
+          <Grid container>
+                <Grid item xs>
+                  <Link href="#" variant="body2">
+                    Forgot password?
+                  </Link>
+                </Grid>
+                <Grid item>
+                  <Link to="/signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
+              </Grid>
+        </form>
+        </div>
+    </Container>
   );
 };
 
