f7111f1ec1d86ac136fcba94aa2a1946b03832b9
Risa Takata <beat1422@icloud.com>
2020-12-04 21:19:44 -0800

    ADD: #32 add the useEffect to get the album data from firestore

--- src/context/AlbumContext.js
+++ src/context/AlbumContext.js
@@ -1,8 +1,31 @@
-import React, { createContext } from "react";
+import React, { useState,createContext,useContext,useEffect } from "react";
+import {AuthContext} from "../context/AuthContext"
 
 export const AlbumContext = createContext();
 
+
+
 const AlbumProvider = (children) => {
+  const [albums, setAlbums] = useState([])
+
+  const { currentUser } = useContext(AuthContext);
+
+  useEffect(async () => {
+    let albumsRef = await
+    db.collection("users")
+    .doc(currentUser.uid)
+    .collection("albums");
+
+    albumsRef.onSnapshot(snapShot => {
+    let tempAlbums = [];
+    snapShot.forEach(doc => {
+    tempAlbums.push(doc.data())
+    })
+    setAlbums(tempAlbums)
+    });
+  },[])
+
+
   return (
     <AlbumContext.Provider>{children}</AlbumContext.Provider>
   )
