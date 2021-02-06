bd931c6b1044b4da3d7ad0900f54cb66dfe0b5a6
Risa Takata <beat1422@icloud.com>
2021-02-06 05:56:44 -0800

    FIX: 消したファイルの後遺症的なエラーを処理した

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,7 +12,6 @@ import Card from "@material-ui/core/Card"
 import CardMedia from "@material-ui/core/CardMedia"
 import CardContent from "@material-ui/core/CardContent"
 import SearchBar from "../components/SearchBar"
-import AddNewAlbumButton from "../components/buttons/NewAlbumButton"
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import Img from "../assets/img/homeTop.png"
 import Button from '@material-ui/core/Button';
