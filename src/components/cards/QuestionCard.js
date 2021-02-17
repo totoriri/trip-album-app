4971a2ccc452ce5a427ccee8f90e4a6624d0b8ff
Risa Takata <beat1422@icloud.com>
2021-02-17 13:51:48 -0800

    FIX: インデントの調整やコード整形

--- src/components/cards/QuestionCard.js
+++ src/components/cards/QuestionCard.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
-import { useRouteMatch, Link,useHistory } from "react-router-dom";
-import { NewPhotoForm } from "../../pages/forms/NewPhotoForm";
+import React, { useState, useEffect } from 'react';
+import { useRouteMatch, Link, useHistory } from 'react-router-dom';
+import { NewPhotoForm } from '../../pages/forms/NewPhotoForm';
 // import { app } from "../base";
 import { withStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
@@ -8,9 +8,9 @@ import Rating from '@material-ui/lab/Rating';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Typography from '@material-ui/core/Typography';
-import Grid from '@material-ui/core/Grid'
-import Card from '@material-ui/core/Card'
-import CardMedia from '@material-ui/core/CardMedia'
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardMedia from '@material-ui/core/CardMedia';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Paper from '@material-ui/core/Paper';
 import PropTypes from 'prop-types';
@@ -25,7 +25,7 @@ import ShoppingBasket from '@material-ui/icons/ShoppingBasket';
 import ThumbDown from '@material-ui/icons/ThumbDown';
 import ThumbUp from '@material-ui/icons/ThumbUp';
 import Box from '@material-ui/core/Box';
-import { defaultQuestions } from "../../pages/DefaultQuestions"
+import { defaultQuestions } from '../../pages/DefaultQuestions';
 import CardHeader from '@material-ui/core/CardHeader';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
@@ -35,7 +35,7 @@ import CardActions from '@material-ui/core/CardActions';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import clsx from 'clsx';
 import Collapse from '@material-ui/core/Collapse';
-import ModalForm from "../modals/ModalForm"
+import ModalForm from '../modals/ModalForm';
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -43,32 +43,31 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-
   },
   travelTitle: {
-    marginTop:theme.spacing(5)
+    marginTop: theme.spacing(5),
   },
   root: {
     // flexGrow: 1,
     width: '100%',
     backgroundColor: theme.palette.background.paper,
-    justifyContent: "center",
-    margin: "0 auto"
+    justifyContent: 'center',
+    margin: '0 auto',
   },
   tab: {
-    "& .MuiTabs-flexContainer": {
-      justifyContent: "center" // or black
-    }
+    '& .MuiTabs-flexContainer': {
+      justifyContent: 'center', // or black
+    },
   },
   footer: {
-    textAlign:"center"
+    textAlign: 'center',
   },
   cardHeader: {
-    variant:"h5",
-    fontweight: "bold",
-    color: "red"
-  }
-}))
+    variant: 'h5',
+    fontweight: 'bold',
+    color: 'red',
+  },
+}));
 
 const StyledRating = withStyles({
   iconFilled: {
@@ -79,53 +78,45 @@ const StyledRating = withStyles({
   },
 })(Rating);
 
-
-
-
-const QuestionCard = ({item,travel}) => {
+const QuestionCard = ({ item, travel }) => {
   const classes = useStyles();
   const [images, setImages] = useState([]);
-   const [travelName, setTravelName] = useState("");
-    const [selectedQuestions, setSelectedQuestions] = useState([])
-    const [isRated,setIsRated] = useState(false)
-   const [expanded, setExpanded] = React.useState(false);
-   const handleExpandClick = () => {
+  const [travelName, setTravelName] = useState('');
+  const [selectedQuestions, setSelectedQuestions] = useState([]);
+  const [isRated, setIsRated] = useState(false);
+  const [expanded, setExpanded] = React.useState(false);
+  const handleExpandClick = () => {
     setExpanded(!expanded);
-   };
+  };
 
-    const onRatingChange = () => {
-    setIsRated(!isRated)
-    console.log(isRated)
-    }
+  const onRatingChange = () => {
+    setIsRated(!isRated);
+    console.log(isRated);
+  };
 
-
-                return (
-                <Card className={classes.root}>
-                  <CardHeader
-                    avatar={
-                      <Avatar aria-label="recipe" className={classes.avatar}>
-                        Q
-                      </Avatar>
-                    }
-                    action={
-                      <IconButton aria-label="settings">
-                        <MoreVertIcon />
-                      </IconButton>
-                    }
-                      // title={item.title}
-                      title={
-                      <Typography gutterBottom variant="h5" component="h2">
-                        {item.title}
-                      </Typography>
-                                }
-                  />
-                  <CardMedia
-                    className={classes.media}
-                    image="/static/images/cards/paella.jpg"
-                    title="Paella dish"
-                  />
-                  <CardActions disableSpacing>
-                  {/* <StyledRating
+  return (
+    <Card className={classes.root}>
+      <CardHeader
+        avatar={
+          <Avatar aria-label="recipe" className={classes.avatar}>
+            Q
+          </Avatar>
+        }
+        action={
+          <IconButton aria-label="settings">
+            <MoreVertIcon />
+          </IconButton>
+        }
+        // title={item.title}
+        title={
+          <Typography gutterBottom variant="h5" component="h2">
+            {item.title}
+          </Typography>
+        }
+      />
+      <CardMedia className={classes.media} image="/static/images/cards/paella.jpg" title="Paella dish" />
+      <CardActions disableSpacing>
+        {/* <StyledRating
                     name="customized-color"
                     defaultValue={0}
                         onChange={onRatingChange}
@@ -134,34 +125,35 @@ const QuestionCard = ({item,travel}) => {
                     icon={<FavoriteIcon fontSize="inherit" />}
                     max={1}
                   /> */}
-                  <StyledRating
-                    name={item.title}
-                    defaultValue={0}
-                    onChange={onRatingChange}
-                    getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
-                    precision={1}
-                    icon={<FavoriteIcon fontSize="inherit" />}
-                    max={1}
-                      />
+        <StyledRating
+          name={item.title}
+          defaultValue={0}
+          value={isRated}
+          onChange={onRatingChange}
+          getLabelText={(value) => `${value} Heart${value !== 1 ? 's' : ''}`}
+          precision={1}
+          icon={<FavoriteIcon fontSize="inherit" />}
+          max={1}
+        />
 
-                    <IconButton
-                      className={clsx(classes.expand, {
-                        [classes.expandOpen]: expanded,
-                      })}
-                      onClick={handleExpandClick}
-                      aria-expanded={expanded}
-                      aria-label="show more"
-                    >
-                      <ExpandMoreIcon />
-                    </IconButton>
-                  </CardActions>
-                  <Collapse in={expanded} timeout="auto" unmountOnExit>
-                    <CardContent>
-                      <ModalForm travel={travel}/>
-                    </CardContent>
-                  </Collapse>
-                </Card>
-  )
-}
+        <IconButton
+          className={clsx(classes.expand, {
+            [classes.expandOpen]: expanded,
+          })}
+          onClick={handleExpandClick}
+          aria-expanded={expanded}
+          aria-label="show more"
+        >
+          <ExpandMoreIcon />
+        </IconButton>
+      </CardActions>
+      <Collapse in={expanded} timeout="auto" unmountOnExit>
+        <CardContent>
+          <ModalForm travel={travel} />
+        </CardContent>
+      </Collapse>
+    </Card>
+  );
+};
 
 export default QuestionCard;
