1cc17e7459be89e24f0b76173b7c96196773eaf7
Risa Takata <beat1422@icloud.com>
2021-02-17 13:58:06 -0800

    DELETE: 不要コードの削除

--- src/components/cards/QuestionCard.js
+++ src/components/cards/QuestionCard.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useRouteMatch, Link, useHistory } from 'react-router-dom';
-import { NewPhotoForm } from '../../pages/forms/NewPhotoForm';
-// import { app } from "../base";
 import { withStyles } from '@material-ui/core/styles';
 import Rating from '@material-ui/lab/Rating';
 import { makeStyles } from '@material-ui/core/styles';
